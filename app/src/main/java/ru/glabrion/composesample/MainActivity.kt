fd385b1a3b4d9722880d2f901263bc1cf6b00fdd
ssulatskov <ssulatskov@rtc.usetech.ru>
2023-07-14 10:28:03 +0300

    список на главном

--- app/src/main/java/ru/glabrion/composesample/MainActivity.kt
+++ app/src/main/java/ru/glabrion/composesample/MainActivity.kt
@@ -1,46 +1,90 @@
 package ru.glabrion.composesample
 
+import android.content.res.Configuration
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import ru.glabrion.composesample.ui.theme.ComposeSampleTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            PostsList(SampleData.list)
+        }
+    }
+}
+
+class Post(
+    @DrawableRes val avatarRes: Int,
+    val name: String,
+    val location: String,
+    @DrawableRes val photoRes: Int
+)
+
+@Composable
+fun PostsList(list: List<Post>) {
+    LazyColumn {
+        items(list) { item ->
+            ItemPost(item)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ItemPost(post: Post) {
+
+    Column(modifier = Modifier.padding(all = 8.dp)) {
+        Row {
+            Image(
+                painter = painterResource(id = post.avatarRes),
+                contentDescription = "profile avatar",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(text = post.name)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = post.location, color = MaterialTheme.colorScheme.secondary)
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = post.photoRes),
+            contentDescription = "image",
+            modifier = Modifier.size(400.dp, 200.dp)
+        )
+
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
-fun GreetingPreview() {
-    ComposeSampleTheme {
-        Greeting("Android")
-    }
+fun ContentPreview() {
+    PostsList(SampleData.list)
 }
---
+++ app/src/main/java/ru/glabrion/composesample/SampleData.kt
@@ -0,0 +1,35 @@
+package ru.glabrion.composesample
+
+class SampleData {
+    companion object {
+
+
+        val list = listOf(
+            Post(
+                avatarRes = R.drawable.ic_launcher_background,
+                name = "name1",
+                location = "location1",
+                photoRes = R.drawable.ic_launcher_background
+            ),
+            Post(
+                avatarRes = R.drawable.ic_launcher_background,
+                name = "name2",
+                location = "location2",
+                photoRes = R.drawable.ic_launcher_background
+            ),
+            Post(
+                avatarRes = R.drawable.ic_launcher_background,
+                name = "name3",
+                location = "location3",
+                photoRes = R.drawable.ic_launcher_background
+            ),
+            Post(
+                avatarRes = R.drawable.ic_launcher_background,
+                name = "name4",
+                location = "location4",
+                photoRes = R.drawable.ic_launcher_background
+            )
+        )
+
+    }
+}
